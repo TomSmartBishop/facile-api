11b18b25308fd7e4ce56a2fb5513bd2741e29d32
mrlongleg1962 <mrlongleg1962@7b07e3c7-c0ca-cf8d-5882-a23c14d376c4>
2012-05-29 22:36:32 +0000

    Added test case for generic methods (generic instance should have a name)

---
+++ Facile/src/test/java/at/pollaknet/api/facile/tests/generics/GenericsTest.java
@@ -0,0 +1,48 @@
+package at.pollaknet.api.facile.tests.generics;
+
+import at.pollaknet.api.facile.Facile;
+import at.pollaknet.api.facile.symtab.symbols.Method;
+import at.pollaknet.api.facile.symtab.symbols.MethodSignature;
+import at.pollaknet.api.facile.symtab.symbols.Type;
+import at.pollaknet.api.facile.symtab.symbols.TypeRef;
+import at.pollaknet.api.facile.symtab.symbols.TypeSpec;
+import at.pollaknet.api.facile.symtab.symbols.scopes.Assembly;
+import junit.framework.TestCase;
+
+public class GenericsTest extends TestCase
+{
+    public void testSystemActivator()
+    {
+        try
+        {
+            Assembly assembly = Facile.loadAssembly("assemblies/v4.0.4.0.0.0_64/mscorlib.dll");
+
+            for (Type type : assembly.getAllTypes())
+            {
+                if (type.getFullQualifiedName().equals("System.Activator"))
+                {
+                    for (Method m : type.getMethods())
+                    {
+                        MethodSignature sig = m.getMethodSignature();
+                        TypeRef ref = sig.getReturnType();
+
+                        if (ref instanceof TypeSpec)
+                        {
+                            TypeSpec spec = (TypeSpec) ref;
+
+                            if (spec.isGenericInstance())
+                            {
+                                assertNotNull(spec.getName());
+                            }
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            fail(e.getMessage());
+        }
+    }
+}
